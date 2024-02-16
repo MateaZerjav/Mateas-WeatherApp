32079601dd3e20507d9972b469b939ecb8c541b8
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-02-16 13:35:11 +0100

    Added basic Search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function searchHandle(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchHandle);
